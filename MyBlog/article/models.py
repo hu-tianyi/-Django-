748da9200491ace2bd29611eaff9e8a7dbb5b793
Unknown <hu_tianyi@outlook.com>
2018-02-07 19:40:01 +0800

    Setup the model of the app "article"

--- MyBlog/article/models.py
+++ MyBlog/article/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length = 100)  # the title of the blog
+    category = models.CharField(max_length = 50, blank = True)  # the label of the blog
+    date_time = models.DateTimeField(auto_now_add = True)  # the publish time of the blog
+    content = models.TextField(blank = True, null = True)  # the content of the blog
+
+    def _unicode_(self):
+        return self.title
+
+    class Meta: # be ordered by time
+        ordering = ['-date_time']
