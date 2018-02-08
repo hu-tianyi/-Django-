8888bfca56b1b0c8527a94337087928d9b9da792
Unknown <hu_tianyi@outlook.com>
2018-02-08 16:28:42 +0800

    add search

--- MyBlog/MyBlog/urls.py
+++ MyBlog/MyBlog/urls.py
@@ -29,4 +29,5 @@ urlpatterns = urlpatterns = [
     url(r'^archives/$', views.archives, name = 'archives'),
     url(r'^aboutme/$', views.about_me, name = 'about_me'),
     url(r'^tag(?P<tag>\w+)/$', views.search_tag, name = 'search_tag'),
+    url(r'^search/$', views.blog_search, name = 'search'),
 ]
